aa6bc647e9aa7ea25feafe9d4fc7c1efe40258f6
dvkonovalov <dvkonovalov@avito.ru>
2024-10-22 09:37:18 +0300

    Init files

---
+++ signer.go
@@ -0,0 +1,19 @@
+package main
+
+// ExecutePipeline
+func ExecutePipeline(jobs ...job) {
+}
+
+// SingleHash
+func SingleHash(in, out chan interface{}) {
+
+}
+
+// MultiHash
+func MultiHash(in, out chan interface{}) {
+
+}
+
+// CombineResults
+func CombineResults(in, out chan interface{}) {
+}
